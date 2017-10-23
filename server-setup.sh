21538e90f34523eddb531f6a5811f5641573851a
rodneyriley <mail@rodneyriley.co.uk>
2017-10-23 13:39:41 +0100

    Added handling for www and non www domains

--- server-setup.sh
+++ server-setup.sh
@@ -45,13 +45,17 @@ read COUNT
 
 for (( i=1; i<=COUNT; i++ ))
 do  
-   echo "Please enter domain $i"
+  echo "Please enter domain $i"
    read DOMAIN[i]
 done
 
 for (( i=1; i<=COUNT; i++ ))
 do  
-    WWW_DOMAIN[i]=www.${DOMAIN[i]}
+  echo "Would you like a www version of ${DOMAIN[i]}"
+   read -p "(y/n)?" CONT
+   if [ "$CONT" = "y" ]; then
+   WWW_DOMAIN[i]=www.${DOMAIN[i]}
+   fi 
 done
 
 #------------------------------------------------------------------------------------
@@ -115,7 +119,8 @@ mkdir /var/www/${DOMAIN[i]}/.well-known
 echo -e "<VirtualHost *:8080>\nServerName ${DOMAIN[i]}\nServerAlias www.${DOMAIN[i]}\nDocumentRoot /var/www/${DOMAIN[i]}\n<Directory /var/www/${DOMAIN[i]}>\nAllowOverride All\n</Directory>\n</VirtualHost>" > /etc/apache2/sites-available/${DOMAIN[i]}.conf
 a2ensite ${DOMAIN[i]}
 done
-
+chown -R www-data /var/www
+chgrp -R www-data /var/www
 systemctl reload apache2
 
 #------------------------------------------------------------------------------------
@@ -173,18 +178,24 @@ echo -e "ssl_protocols TLSv1.1 TLSv1.2;\nssl_prefer_server_ciphers on;\nssl_ciph
 for (( i=1; i<=COUNT; i++ ))
 do
 echo -e "ssl_certificate /etc/letsencrypt/live/${DOMAIN[i]}/fullchain.pem;\nssl_certificate_key /etc/letsencrypt/live/${DOMAIN[i]}/privkey.pem;" > /etc/nginx/snippets/ssl-${DOMAIN[i]}.conf
-letsencrypt certonly -a webroot --webroot-path=/var/www/${DOMAIN[i]} -d ${DOMAIN[i]} -d www.${DOMAIN[i]}
+letsencrypt certonly -a webroot --webroot-path=/var/www/${DOMAIN[i]} -d ${DOMAIN[i]} 
+if [[ ${WWW_DOMAIN[i]} ]]; then
+echo -e "ssl_certificate /etc/letsencrypt/live/${WWW_DOMAIN[i]}/fullchain.pem;\nssl_certificate_key /etc/letsencrypt/live/${WWW_DOMAIN[i]}/privkey.pem;" > /etc/nginx/snippets/ssl-${WWW_DOMAIN[i]}.conf
+letsencrypt certonly -a webroot --webroot-path=/var/www/${DOMAIN[i]} -d ${WWW_DOMAIN[i]}
+fi
 done
-openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
+
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 rm /etc/nginx/sites-available/apache
 touch /etc/nginx/sites-available/apache
 for (( i=1; i<=COUNT; i++ ))
 do 
-
-echo -e "server {\n  listen 80;\n  listen 443 ssl http2;\n include snippets/ssl-${DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${DOMAIN[i]} ${WWW_DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
-
+echo -e "server {\n  listen 80;\n  listen 443 ssl http2;\n include snippets/ssl-${DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
+if [[ ${WWW_DOMAIN[i]} ]]; then
+echo -e "server {\n  listen 80;\n  listen 443 ssl http2;\n include snippets/ssl-${WWW_DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${WWW_DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
+fi
 done
 service nginx reload
 
