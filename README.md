aab36bf700249a5931d55e65b2361fc5b5847fde
Rodney Riley <rodneyriley@Rodneys-MacBook-Pro-2.local>
2019-11-18 12:02:23 +0000

    Updated add url script for 18.04 setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ubuntu-webserver-setup
 Script that creates a web server running Nginx / Apache / php / imagemagick / htmltopdf / Lets Encrypt. Script automatically configures basic security and any number of virtual hosts including the process of creating SSL certificates. Finally the script sets up a user for SSH and joins them to the www-data group. Intended as a starting point and enhancements / comments very welcome.
+
+Upload the scripts can be done in lots of ways but the following command works well
+
+scp server-setup.sh root@your.server.ip.here:.
+
+Once uploaded set it to be executable by whatever user or group you choose. 755 works well generally (chmod 755 server-setup.sh)
+
--- add-url.sh
+++ add-url.sh
@@ -4,8 +4,12 @@
 # Update repos and server
 #------------------------------------------------------------------------------------
 
-/usr/bin/apt-get -y update
-/usr/bin/apt-get -y upgrade
+add-apt-repository universe
+apt -y install software-properties-common
+add-apt-repository -y ppa:ondrej/php
+apt-get -y update
+apt-get -y upgrade
+apt -y autoremove
 
 #------------------------------------------------------------------------------------
 # Gather domain names into array
@@ -30,27 +34,36 @@ do
 done
 
 #------------------------------------------------------------------------------------
-# Create virtual hosts for apache ($a append after last line, i1 before line 1) 
+# Get public IP of server
+#------------------------------------------------------------------------------------
+
+PUBLIC_IP=`wget http://ipecho.net/plain -O - -q ; echo`
+echo $PUBLIC_IP
+
+#------------------------------------------------------------------------------------
+# Create virtual hosts for apache 
 #------------------------------------------------------------------------------------
 
 for (( i=1; i<=COUNT; i++ ))
 do  
 mkdir /var/www/${DOMAIN[i]}
 mkdir /var/www/${DOMAIN[i]}/.well-known
-echo -e "<VirtualHost *:8080>\nServerName ${DOMAIN[i]}\nServerAlias www.${DOMAIN[i]}\nDocumentRoot /var/www/${DOMAIN[i]}\n<Directory /var/www/${DOMAIN[i]}>\nAllowOverride All\n</Directory>\n</VirtualHost>" > /etc/apache2/sites-available/${DOMAIN[i]}.conf
+touch /var/www/${DOMAIN[i]}/.htaccess
+echo -e "<VirtualHost *:8080>\nServerName ${DOMAIN[i]}\nServerAlias www.${DOMAIN[i]}\nDocumentRoot /var/www/${DOMAIN[i]}\n<Directory /var/www/${DOMAIN[i]}>\nAllowOverride None\nInclude /var/www/${DOMAIN[i]}/.htaccess\n</Directory>\n</VirtualHost>" >> /etc/apache2/sites-available/${DOMAIN[i]}.conf
 a2ensite ${DOMAIN[i]}
-chown -R www-data /var/www/${DOMAIN[i]}
-chgrp -R www-data /var/www/${DOMAIN[i]}
 done
+chown -R www-data /var/www
+chgrp -R www-data /var/www
+chmod -R 755 /var/www
 systemctl reload apache2
 
 #------------------------------------------------------------------------------------
-# Installing and Configuring Nginx for Apache virtual hosts
+# Configuring Nginx for Apache virtual hosts
 #------------------------------------------------------------------------------------
+
 
 echo -e "server {\n  listen 80;\n    server_name ${DOMAIN[@]} ${WWW_DOMAIN[@]};\nlocation / {\nproxy_pass http://127.0.0.1:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
 
-ln -s /etc/nginx/sites-available/apache /etc/nginx/sites-enabled/apache
 service nginx reload
 
 #------------------------------------------------------------------------------------
@@ -67,11 +80,14 @@ letsencrypt certonly -a webroot --webroot-path=/var/www/${DOMAIN[i]} -d ${WWW_DO
 fi
 done
 
+
 for (( i=1; i<=COUNT; i++ ))
 do 
-echo -e "server {\n  listen 80;\n  listen 443 ssl http2;\n include snippets/ssl-${DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
+echo -e "server {\n  listen 443 ssl http2;\n include snippets/ssl-${DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache	
+echo -e "server {\n  listen 80;\n  listen [::]:80;\n  server_name ${DOMAIN[i]};\n  return 301 https://${DOMAIN[i]}$request_uri;\n }" >> /etc/nginx/sites-available/apache	
 if [[ ${WWW_DOMAIN[i]} ]]; then
-echo -e "server {\n  listen 80;\n  listen 443 ssl http2;\n include snippets/ssl-${WWW_DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${WWW_DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
+echo -e "server {\n  listen 443 ssl http2;\n include snippets/ssl-${WWW_DOMAIN[i]}.conf;\n include snippets/ssl-params.conf;\n server_name ${WWW_DOMAIN[i]};\nlocation / {\nproxy_pass http://$PUBLIC_IP:8080;\n proxy_set_header Host \$host;\n proxy_set_header X-Real-IP \$remote_addr;\n proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n proxy_set_header X-Forwarded-Proto \$scheme;} }" >> /etc/nginx/sites-available/apache
+echo -e "server {\n  listen 80;\n  listen [::]:80;\n  server_name ${WWW_DOMAIN[i]};\n  return 301 https://${WWW_DOMAIN[i]}$request_uri;\n }" >> /etc/nginx/sites-available/apache	
 fi
 done
 service nginx reload
